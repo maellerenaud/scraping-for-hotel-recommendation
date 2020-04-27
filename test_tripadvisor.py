7056f78a7d917cf6f7ca8355877614ceecf914b3
MaelleRenaud <maelle.renaud@student-cs.fr>
2020-04-27 20:33:41 +0200

    Take all characteristics of one hotel

--- test_tripadvisor.py
+++ test_tripadvisor.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tripadvisor import adapt_dates_to_tripadvisor, setup_browser, request, month_difference
+from tripadvisor import *
 
 class TestFunctions(unittest.TestCase):
 
@@ -13,6 +13,7 @@ class TestFunctions(unittest.TestCase):
         self.assertEqual(month_difference('2020-4-27', '2020-4-27'), 0)
         self.assertEqual(month_difference('2020-11-27', '2021-1-4'), 2)
 
+
 class TestSelenium(unittest.TestCase):
 
     def test_request(self):
@@ -22,6 +23,60 @@ class TestSelenium(unittest.TestCase):
         browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
         request(browser, 'Nantes', '2020-5-29', '2020-6-2')
         self.assertEqual(browser.current_url, "https://www.tripadvisor.fr/Hotels-g187198-Nantes_Loire_Atlantique_Pays_de_la_Loire-Hotels.html")
+
+
+class TestGetCharacteristics(unittest.TestCase):
+
+    def test_get_price(self):
+        browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
+        request(browser, 'Rennes', '2020-5-27', '2020-5-28')
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d2064905-Reviews-Novotel_SPA_Rennes_Centre_Gare-Rennes_Ille_et_Vilaine_Brittany.html')
+        time.sleep(1)
+        self.assertEqual(get_price(browser), 176)
+
+    def test_get_grade(self):
+        browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d2064905-Reviews-Novotel_SPA_Rennes_Centre_Gare-Rennes_Ille_et_Vilaine_Brittany.html')
+        time.sleep(1)
+        self.assertEqual(get_grade(browser), (4717/5745, 1149))
+
+    def test_get_location(self):
+        browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d2064905-Reviews-Novotel_SPA_Rennes_Centre_Gare-Rennes_Ille_et_Vilaine_Brittany.html')
+        time.sleep(2)
+        self.assertEqual(get_localisation(browser), 99)
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d1545570-Reviews-Sejours_Affaires_Bretagne_Rennes-Rennes_Ille_et_Vilaine_Brittany.html')
+        time.sleep(2)
+        self.assertEqual(get_localisation(browser), 84)
+
+    def test_get_services_on_services_page(self):
+        browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d2064905-Reviews-Novotel_SPA_Rennes_Centre_Gare-Rennes_Ille_et_Vilaine_Brittany.html')
+        time.sleep(2)
+        plus = browser.find_element_by_xpath('//div[@id="ABOUT_TAB"]/div[1]/div[2]/div[1]/div[3]')
+        plus.click()
+        time.sleep(1)
+        self.assertEqual(get_services_on_services_page(browser), (True, False, False))
+
+    def test_get_services_on_hotel_page(self):
+        browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d2064905-Reviews-Novotel_SPA_Rennes_Centre_Gare-Rennes_Ille_et_Vilaine_Brittany.html')
+        time.sleep(2)
+        self.assertEqual(get_services_on_hotel_page(browser, True), (False, True, True))
+
+
+    def test_get_services(self):
+        browser = setup_browser(r"/home/peach/PycharmProjects/scraping/chromedriver")
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d2064905-Reviews-Novotel_SPA_Rennes_Centre_Gare-Rennes_Ille_et_Vilaine_Brittany.html')
+        self.assertEqual(get_services(browser), (True, True, True))
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d1545570-Reviews-Sejours_Affaires_Bretagne_Rennes-Rennes_Ille_et_Vilaine_Brittany.html')
+        self.assertEqual(get_services(browser), (True, False, False))
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d12099897-Reviews-Hisseo-Rennes_Ille_et_Vilaine_Brittany.html')
+        self.assertEqual(get_services(browser), (False, False, False))
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187103-d308006-Reviews-Auberge_de_Jeunesse_de_Rennes-Rennes_Ille_et_Vilaine_Brittany.html')
+        self.assertEqual(get_services(browser), (False, False, False))
+        browser.get('https://www.tripadvisor.fr/Hotel_Review-g187147-d231051-Reviews-Hotel_Harvey-Paris_Ile_de_France.html')
+        self.assertEqual(get_services(browser), (True, True, True))
 
 
 if __name__ == '__main__':
--- tripadvisor.py
+++ tripadvisor.py
@@ -1,24 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import time
 from datetime import date
 import os
 
-#Introduction necessaire au module selenium
-option = webdriver.ChromeOptions()
-option.add_argument("--incognito")
-option.add_argument("--start-maximised") #"kiosk" sur mac
-prefs = {"profile.managed_default_content_settings.images": 2, 'disk-cache-size': 4096 }
-option.add_experimental_option('prefs', prefs)
-#browser = webdriver.Chrome(executable_path=str(os.getcwd())+"\chromedriver", options=option) #!!! Executable à télécharger et path à changer
-browser = webdriver.Chrome(executable_path=r"/home/peach/PycharmProjects/scraping/chromedriver", options=option)
-
-
-def adapt_dates_to_tripadvisor(date):
-    """ date in format "year-month-day" """
-    list = date.split('-')
-    list[1] = str(int(list[1]) - 1)
-    return '-'.join(list)
-
+### Set up browser
 
 def setup_browser(path_chromedriver):
     option = webdriver.ChromeOptions()
@@ -29,6 +17,14 @@ def setup_browser(path_chromedriver):
     #browser = webdriver.Chrome(executable_path=str(os.getcwd())+"\chromedriver", options=option) #!!! Executable à télécharger et path à changer
     browser = webdriver.Chrome(executable_path=path_chromedriver, options=option)
     return browser
+
+### Connect to tripadvisor and make request
+
+def adapt_dates_to_tripadvisor(date):
+    """ date in format "year-month-day" """
+    list = date.split('-')
+    list[1] = str(int(list[1]) - 1)
+    return '-'.join(list)
 
 def chose_town(search_bar, town):
     picker_town = search_bar.find_element_by_xpath('./div[1]/div[1]/span[1]/input[1]')
@@ -55,7 +51,6 @@ def chose_both_dates(browser, search_bar, arrival_date, departure_date):
     chose_date(browser, arrival_date)
     chose_date(browser, departure_date)     # No need to change month, we can reserve only for 30 days maximum
 
-
 def chose_guests(browser, search_bar):
     open_guests_choice = search_bar.find_element_by_xpath('//div[@data-prwidget-name="ibex_trip_search_rooms_guests"]')
     open_guests_choice.click()
@@ -71,7 +66,6 @@ def chose_guests(browser, search_bar):
         children_ages_wrap.find_element_by_xpath('./span[{}]/span[1]/div[1]/span[1]/ul[2]/li[@data-val="10"]'.format(str(i))).click()
         time.sleep(0.5)
 
-
 def request(browser, town, arrival_date, departure_date):
     browser.get('https://www.tripadvisor.fr/Hotels')
     time.sleep(1)
@@ -82,5 +76,80 @@ def request(browser, town, arrival_date, departure_date):
     search_button = browser.find_element_by_xpath('//button[@id="SUBMIT_HOTELS"]')
     search_button.click()
     time.sleep(1)
+
+### Get charateristics of one hotel and save them
+
+def get_price(browser):
+    best_offer = browser.find_element_by_xpath('//div[@class="hotels-hotel-offers-DominantOffer__price--D-ycN"]')
+    price = int(best_offer.text[:-1])
+    return price
+
+def get_grade(browser):
+    votes = browser.find_elements_by_xpath('//ul[@class="location-review-review-list-parts-ReviewFilter__filter_table--1H9KD"]/li/span[2]')
+    grade_votes = {}
+    grade = 5
+    for element in votes:
+        grade_votes[grade] = int(element.text)
+        grade -= 1
+    nb_votes = grade_votes[1] + grade_votes[2] + grade_votes[3] + grade_votes[4] + grade_votes[5]
+    average =  (grade_votes[1] + grade_votes[2]*2 + grade_votes[3]*3 + grade_votes[4]*4 + grade_votes[5]*5) / (nb_votes*5)
+    return average, nb_votes
+
+def get_localisation(browser):
+    indice = browser.find_element_by_xpath('//div[@id="LOCATION"]/div[2]/div[1]/span[1]').text
+    return int(indice)
+
+def get_services_on_services_page(browser):
+    wifi, clim, minibar = False, False, False
+    services = browser.find_elements_by_xpath('//div[@class="hotels-hr-about-amenities-AmenitiesModal__group--3nudN"]/div')
+    for service in services:
+        wifi = wifi or service.text == 'Wi-Fi'
+        clim = clim or service.text == 'Climatisation'
+        minibar = minibar or service.text == 'Minibar'
+    quit = browser.find_element_by_xpath('//div[@class="_2EFRp_bb _3IWKziRc _3ptEwvMl"][@role="button"]')
+    quit.click()
+    return wifi, clim, minibar
+
+def get_services_on_hotel_page(browser, room_services):
+    wifi, clim, minibar = False, False, False
+    if room_services:
+        services = browser.find_elements_by_xpath('//div[@id="ABOUT_TAB"]/div[1]/div[2]/div[1]/div[@class="hotels-hr-about-amenities-AmenityGroup__amenitiesList--3MdFn"][2]/div')
+    else:
+        services = browser.find_elements_by_xpath('//div[@id="ABOUT_TAB"]/div[1]/div[2]/div[1]/div[@class="hotels-hr-about-amenities-AmenityGroup__amenitiesList--3MdFn"][1]/div')
+    for service in services:
+        wifi = wifi or service.text == 'Wi-Fi'
+        clim = clim or service.text == 'Climatisation'
+        minibar = minibar or service.text == 'Minibar'
+    return wifi, clim, minibar
+
+def get_services(browser):
+    wifi, clim, minibar = False, False, False
+    # Get hotel services
+    try:
+        plus = browser.find_element_by_xpath('//div[@id="ABOUT_TAB"]/div[1]/div[2]/div[1]/div[3]')
+        plus.click()
+        time.sleep(1)
+        wifi1, clim1, minibar1 = get_services_on_services_page(browser)
+        wifi, clim, minibar = wifi or wifi1, clim or clim1, minibar or minibar1
+    except:
+        try:
+            wifi2, clim2, minibar2 = get_services_on_hotel_page(browser, False)
+            wifi, clim, minibar = wifi or wifi2, clim or clim2, minibar or minibar2
+        except:
+            pass
+    # Get room services
+    try:
+        plus = browser.find_element_by_xpath('//div[@id="ABOUT_TAB"]/div[1]/div[2]/div[1]/div[6]')
+        plus.click()
+        time.sleep(1)
+        wifi3, clim3, minibar3 = get_services_on_services_page(browser)
+        wifi, clim, minibar = wifi or wifi3, clim or clim3, minibar or minibar3
+    except:
+        try:
+           wifi4, clim4, minibar4 = get_services_on_hotel_page(browser, True)
+           wifi, clim, minibar = wifi or wifi4, clim or clim4, minibar or minibar4
+        except:
+            pass
+    return wifi, clim, minibar
 
 
