b8bdf3ebb67eb912599ae31a068cbc2309260f63
MaelleRenaud <maelle.renaud@student-cs.fr>
2020-04-30 11:04:00 +0200

    Calculate points by criteria and ranking function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import unittest
 
 from utils import *
 
-class TestUtils(unittest.TestCase):
+class TestUtilsForDatbase(unittest.TestCase):
 
     def test_normalize_string(self):
         self.assertEqual(normalize_string('Hotel The Originals Rennes Sud La Chaussairie'), 'hotel the originals rennes sud la chaussairie')
@@ -26,6 +26,39 @@ class TestUtils(unittest.TestCase):
         address = normalize_string('Brit Hotel du Parc - Fougères, 5 Zone de La Pilais 35133 Lécousse FR, Lécousse')
         self.assertEqual(get_distance('Romagné', address), 3.1)
 
+
+class TestUtilsForPoints(unittest.TestCase):
+
+    def test_points_price(self):
+        self.assertEqual(points_price(35, 0, 100), 21)
+        self.assertEqual(points_price(75.99, 0, 100), 9)
+        self.assertEqual(points_price(0, 0, 100), 30)
+        self.assertEqual(points_price(100, 0, 100), 3)
+
+    def test_points_grade(self):
+        self.assertEqual(points_grade(0.8756, 0, 1), 17.51)
+        self.assertEqual(points_grade(0.8, 0.7, 0.9), 10)
+
+    def test_points_distance(self):
+        self.assertEqual(points_distance(46, 0, 100), 10.8)
+        self.assertEqual(points_distance(30, 20, 40), 10)
+        self.assertEqual(points_distance(20, 20, 40), 20)
+        self.assertEqual(points_distance(40, 20, 40), 0)
+
+    def test_points_wifi(self):
+        self.assertEqual(points_wifi(True), 15)
+        self.assertEqual(points_wifi(False), 0)
+
+    def test_points_minibar(self):
+        self.assertEqual(points_minibar(True), 5)
+        self.assertEqual(points_minibar(False), 0)
+
+    def test_points_clim(self):
+        self.assertEqual(points_clim(True), 10)
+        self.assertEqual(points_clim(False), 0)
+
+    def test_ranking_function(self):
+        self.assertEqual(ranking_function(80, 50, 100, 8, 7, 9, 2, 0, 4, True, False, False), 50)
 
 if __name__ == '__main__':
     unittest.main()
--- utils.py
+++ utils.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
+### Set up browser
+
 def setup_browser(path_chromedriver):
     option = webdriver.ChromeOptions()
     option.add_argument("--incognito")
@@ -12,6 +14,8 @@ def setup_browser(path_chromedriver):
     #browser = webdriver.Chrome(executable_path=str(os.getcwd())+"\chromedriver", options=option) #!!! Executable à télécharger et path à changer
     browser = webdriver.Chrome(executable_path=path_chromedriver, options=option)
     return browser
+
+### Utils to fill database
 
 def normalize_string(string):
     lower_case = string.lower()
@@ -43,3 +47,42 @@ def get_distance(town, address):
     wait = WebDriverWait(browser, 10)
     distance = wait.until(ec.visibility_of_element_located((By.XPATH, '//div[@id="section-directions-trip-0"]/div[2]/div[1]/div[1]/div[2]/div[1]'))).text
     return convert_distance_string_into_float(distance)
+
+### Attribute points to each criteria to get a ranking function on 100 points
+
+points_sharing = {'price': 30, 'grade': 20, 'distance': 20, 'wifi': 15, 'minibar': 5, 'clim': 10}
+
+def points_price(price, price_min, price_max):
+    slices = [price_min + (price_max - price_min) * k / 10 for k in range(11)]
+    slice_indice = 0
+    while slices[slice_indice + 1] < price:
+        slice_indice += 1
+    points = points_sharing['price'] * (10 - slice_indice) / 10
+    return round(points, 2)
+
+def points_grade(grade, grade_min, grade_max):
+    '''grade : float between 0 and 1'''
+    relative_grade = (grade - grade_min) / (grade_max - grade_min)
+    points = points_sharing['grade'] * relative_grade
+    return round(points, 2)
+
+def points_distance(distance, distance_min, distance_max):
+    relative_distance = (distance - distance_min) / (distance_max - distance_min)
+    points = points_sharing['distance'] * (1 - relative_distance)
+    return round(points, 2)
+
+def points_wifi(wifi):
+    return wifi * points_sharing['wifi']
+
+def points_minibar(minibar):
+    return minibar * points_sharing['minibar']
+
+def points_clim(clim):
+    return clim * points_sharing['clim']
+
+def ranking_function(price, price_min, price_max, grade, grade_min, grade_max, distance, distance_min, distance_max, wifi, minibar, clim):
+    pts_price = points_price(price, price_min, price_max)
+    pts_grade = points_grade(grade, grade_min, grade_max)
+    pts_distance = points_distance(distance, distance_min, distance_max)
+    pts_services = points_wifi(wifi) + points_minibar(minibar) + points_clim(clim)
+    return pts_price + pts_grade + pts_distance + pts_services
